df19d3152a4985f4b17c4995e98c28d2f482c241
Champerson <violentoleg@gmail.com>
2020-02-16 02:14:44 +0200

    Refactor DroidBattleViewer and .gitignore

--- DroidBattles/src/com/gmail/violentoleg/droid/battles/game/viewer/DroidBattleViewer.java
+++ DroidBattles/src/com/gmail/violentoleg/droid/battles/game/viewer/DroidBattleViewer.java
@@ -5,23 +5,29 @@ import com.gmail.violentoleg.droid.battles.game.model.Droid;
 
 public class DroidBattleViewer {
 
+    private static final String BATTLE_PARTICIPANTS_HEADER_MESSAGE = "Participants of the battle: ";
+    private static final String BATTLE_HEADER_BEGIN_MESSAGE = System.lineSeparator() + " THE BATTLE BEGIN!";
+    private static final String RESULT_OF_THE_ROUND_MESSAGE = "\nResult of the round ";
+    private static final String FIRST_FIGHTER_HEALTH_TRACK_MESSAGE = "First fighter HP: ";
+    private static final String SECOND_FIGHTER_HEALTH_TRACK_MESSAGE = "Second fighter HP: ";
+
     public void printDroidSpecification(Droid droid) {
         System.out.println(droid);
     }
 
     public void printDroidBattleHeader(Droid firstFighter, Droid secondFighter) {
-        System.out.println("Participants of the battle: ");
+        System.out.println(BATTLE_PARTICIPANTS_HEADER_MESSAGE);
         printDroidSpecification(firstFighter);
         printDroidSpecification(secondFighter);
-        System.out.println("\n Let's start!!!");
+        System.out.println(BATTLE_HEADER_BEGIN_MESSAGE);
     }
 
     public void printTitleOfRound(int round) {
-        System.out.println("\nResult of the round " + round);
+        System.out.println(RESULT_OF_THE_ROUND_MESSAGE + round);
     }
 
     public void printResultOfRound(Droid firstFighter, Droid secondFighter) {
-        System.out.println("First fighter HP: " + firstFighter.getHealth() + "\n" +
-                "Second fighter HP: " + secondFighter.getHealth());
+        System.out.println(FIRST_FIGHTER_HEALTH_TRACK_MESSAGE + firstFighter.getHealth() + System.lineSeparator() +
+                SECOND_FIGHTER_HEALTH_TRACK_MESSAGE + secondFighter.getHealth());
     }
 }
